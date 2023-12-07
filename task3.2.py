3f66092619d8e2633593a688c94fcbb5ff785934
qmeeko <KaVVa11M1k0@gmail.com>
2023-12-07 15:53:22 +0300

    доработа задание 5.3.2

--- task3.2.py
+++ task3.2.py
@@ -2,8 +2,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 #1
-x=np.linspace(1,16,16)
-y=np.linspace(1,16,16)
+x=np.linspace(0,16,16)
+y=np.linspace(0,16,16)
 x, y = np.meshgrid(x, y)
 z=np.power(x,0.25)+np.power(y,0.25)
 
@@ -17,8 +17,8 @@ ax.set_zlabel('Z')
 plt.show()
 
 #2
-x=np.linspace(1,10,10)
-y=np.linspace(1,10,10)
+x=np.linspace(0,10,10)
+y=np.linspace(0,10,10)
 x, y = np.meshgrid(x, y)
 z=np.power(x,2)-np.power(y,2)
 
@@ -35,8 +35,8 @@ ax.view_init( azim=60)
 plt.show()
 
 #3
-x=np.linspace(1,10,10)
-y=np.linspace(1,10,10)
+x=np.linspace(0,10,10)
+y=np.linspace(0,10,10)
 x, y = np.meshgrid(x, y)
 z=2*x+3*y
 
@@ -50,8 +50,8 @@ ax.set_zlabel('Z')
 plt.show()
 
 #4
-x=np.linspace(1,10,10)
-y=np.linspace(1,10,10)
+x=np.linspace(0,10,10)
+y=np.linspace(0,10,10)
 x, y = np.meshgrid(x, y)
 z=np.power(x,2)+np.power(y,2)
 
@@ -68,8 +68,8 @@ ax.view_init( azim=270)
 plt.show()
 
 #5
-x=np.linspace(1,10,10)
-y=np.linspace(1,10,10)
+x=np.linspace(0,10,10)
+y=np.linspace(0,10,10)
 x, y = np.meshgrid(x, y)
 z=2+2*x+2*y-np.power(x,2)-np.power(y,2)
 
